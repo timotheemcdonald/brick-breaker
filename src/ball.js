3fa88fe3bf916ca5f2480fd4f031c5ed07f4847b
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-09 02:07:28 -0500

    finished

--- src/ball.js
+++ src/ball.js
@@ -1,12 +1,18 @@
+import { detectCollision } from './collisionDetection.js'
+
 export default class Ball {
     constructor(game){
         this.image = document.getElementById('img_ball')
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
         this.game = game;
-        this.position = {x: 10, y: 10}
-        this.speed = {x: 4, y: 2}
         this.size = 16;
+        this.reset();
+    }
+
+    reset(){
+        this.position = {x: 10, y: 400}
+        this.speed = {x: 6, y: -4}
     }
 
     draw(context){
@@ -22,20 +28,22 @@ export default class Ball {
             this.speed.x = -this.speed.x;
         }
 
-        if(this.position.y + this.size > this.gameHeight || this.position.y < 0){
+        //top
+        if(this.position.y < 0){
             this.speed.y = -this.speed.y;
         }
 
-        let bottomOfBall = this.position.y + this.size;
-        let topOfPaddle = this.game.paddle.position.y;
-        let leftSideOfPaddle = this.game.paddle.position.x;
-        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
-
-        if( bottomOfBall >= topOfPaddle 
-            && this.position.x >= leftSideOfPaddle 
-            && this.position.x + this.size <= rightSideOfPaddle){
-            this.speed.y = -this.speed.y;
-            this.position.y = this.game.paddle.position.y - this.size;
+        //bottom
+        if(this.position.y + this.size > this.gameHeight) {
+            this.game.lives--;
+            this.reset();
         }
+
+        if(detectCollision(this, this.game.paddle)) {
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size
+        }
+
+
     }
 }
--- src/game.js
+++ src/game.js
@@ -1,34 +1,113 @@
 import Paddle from "./paddle.js"
 import InputHandler from "./input.js"
 import Ball from "./ball.js"
+import Brick from "./brick.js"
+import { buildLevel, level1, level2} from "./levels.js"
+
+const GAMESTATE = {
+    PAUSED: 0,
+    RUNNING: 1,
+    MENU: 2,
+    GAMEOVER: 3,
+    NEWLEVEL: 4
+}
 
 export default class Game {
 
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
+        this.gamestate = GAMESTATE.MENU;
+        this.paddle = new Paddle(this);
+        this.ball = new Ball(this);
+        this.gameObjects = [];
+        this.bricks = [];
+        this.lives = 1;
+
+        this.levels = [level1, level2]
+        this.currentLevel = 0;
+
+        new InputHandler(this.paddle, this);
     }
 
 
     start(){
+        if (this.gamestate !== GAMESTATE.MENU &&
+            this.gamestate !== GAMESTATE.NEWLEVEL) return;
 
-        this.paddle = new Paddle(this);
-        this.ball = new Ball(this);
+        this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+        this.ball.reset();
 
         this.gameObjects = [
             this.ball,
-            this.paddle
+            this.paddle,
         ]
     
-        new InputHandler(this.paddle);
+        this.gamestate = GAMESTATE.RUNNING;
     }
 
     update(deltaTime){
-        this.gameObjects.forEach((object) => object.update(deltaTime))
+        if(this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER
+
+        if(this.gamestate === GAMESTATE.PAUSED || 
+            this.gamestate === GAMESTATE.MENU ||
+            this.gamestate === GAMESTATE.GAMEOVER) 
+            return;
+
+        if(this.bricks.length === 0){
+            this.currentLevel++;
+            this.gamestate = GAMESTATE.NEWLEVEL
+            this.start();
+        }
+
+        [...this.gameObjects, ...this.bricks].forEach((object) => object.update(deltaTime));
+
+        this.bricks = this.bricks.filter(brick => !brick.markedForDeletion)
     }
 
     draw(context){
-        this.gameObjects.forEach((object) => object.draw(context))
+        [...this.gameObjects, ...this.bricks].forEach((object) => object.draw(context))
+
+        if (this.gamestate === GAMESTATE.PAUSED) {
+        context.rect(0,0,this.gameWidth,this.gameHeight);
+        context.fillStyle = "rgba(0,0,0,0.25)"
+        context.fill();
+
+        context.font = "30px Poppins";
+        context.fillStyle = "gold";
+        context.textAlign = "center";
+        context.fillText("P A U S E D", this.gameWidth / 2, this.gameHeight / 2)
+        }
+
+        if (this.gamestate === GAMESTATE.MENU) {
+            context.rect(0,0,this.gameWidth,this.gameHeight);
+            context.fillStyle = "slateblue"
+            context.fill();
+    
+            context.font = "30px Poppins";
+            context.fillStyle = "gold";
+            context.textAlign = "center";
+            context.fillText("Press SPACEBAR to START", this.gameWidth / 2, this.gameHeight / 2)
+        }
+
+        if (this.gamestate === GAMESTATE.GAMEOVER) {
+            context.rect(0,0,this.gameWidth,this.gameHeight);
+            context.fillStyle = "slateblue"
+            context.fill();
+    
+            context.font = "30px Poppins";
+            context.fillStyle = "gold";
+            context.textAlign = "center";
+            context.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2)
+        }
+    }
+
+    togglePause() {
+        if(this.gamestate === GAMESTATE.PAUSED) {
+            this.gamestate = GAMESTATE.RUNNING;
+        } else {
+            this.gamestate = GAMESTATE.PAUSED;
+        }
     }
    
 
